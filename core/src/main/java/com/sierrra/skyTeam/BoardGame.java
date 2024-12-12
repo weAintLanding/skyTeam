569c20bd64e3de3ea22caae387fed4cfa21df6bd
Taha <tkhapra@hof-university.de>
2024-12-12 18:20:16 +0100

    literally not a single online solution to this

--- core/src/main/java/com/sierrra/skyTeam/BoardGame.java
+++ core/src/main/java/com/sierrra/skyTeam/BoardGame.java
@@ -2,35 +2,39 @@ package com.sierrra.skyTeam;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricStaggeredTiledMapRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.*;
 
 public class BoardGame extends ApplicationAdapter {
     SpriteBatch batch;
     Texture background;
-    float offsetX, offsetY;
+    Stage stage;
+    Viewport viewport;
 
     @Override
     public void create () {
+        stage = new Stage(new ScreenViewport());
         batch = new SpriteBatch();
         background = new Texture("skyteam.png");
+        viewport = new FitViewport(1280, 720);
     }
 
     @Override
     public void render() {
-        float scaleX = (float) Gdx.graphics.getWidth() / background.getWidth();
-        float scaleY = (float) Gdx.graphics.getHeight() / background.getHeight();
-        float scale = Math.min(scaleX, scaleY);
-
-        float scaledWidth = background.getWidth() * scale;
-        float scaledHeight = background.getHeight() * scale;
-
-        offsetX = (float) (Gdx.graphics.getWidth() - scaledWidth) / 2;
-        offsetY = (float) (Gdx.graphics.getHeight() - scaledHeight) / 2;
-
+        viewport.apply();
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen to avoid flickering
         batch.begin();
-        batch.draw(background, offsetX, offsetY, scaledWidth, scaledHeight);
+        batch.draw(background, 0, 0,
+            Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.end();
+    }
+    @Override
+    public void resize (int width, int height) {
+        viewport.update(width, height, true);
+        batch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
     }
 }
