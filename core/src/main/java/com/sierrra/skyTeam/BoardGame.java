b5c784332b7cedbc97c68f952034aae85f8de78f
Taha <tkhapra@hof-university.de>
2024-12-13 21:37:22 +0100

    added rolling and rendering with a click function that returns player and dice value. resizing has gone to shit again and there is a console error which idk how to fix :)

--- core/src/main/java/com/sierrra/skyTeam/BoardGame.java
+++ core/src/main/java/com/sierrra/skyTeam/BoardGame.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.tiled.renderers.IsometricStaggeredTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.*;
 
@@ -14,6 +13,8 @@ public class BoardGame extends ApplicationAdapter {
     Texture background;
     Stage stage;
     Viewport viewport;
+    Dice dice;
+    boolean isDiceRolled = false;
 
     @Override
     public void create () {
@@ -21,20 +22,39 @@ public class BoardGame extends ApplicationAdapter {
         batch = new SpriteBatch();
         background = new Texture("skyteam.png");
         viewport = new FitViewport(1280, 720);
+        dice = new Dice();
     }
 
     @Override
     public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         viewport.apply();
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen to avoid flickering
         batch.begin();
         batch.draw(background, 0, 0,
             Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        if (!isDiceRolled){
+            dice.rollDice();
+            isDiceRolled = true;
+        }
+        dice.renderDice(batch, true, 100);
+        dice.renderDice(batch, false, 100);
+        if(Gdx.input.isTouched()){
+            float touchX = Gdx.input.getX();
+            float touchY = Gdx.input.getY();
+            touchY = Gdx.graphics.getHeight() - touchY;
+
+            if(dice.isDiceClicked(dice.getCurrentPilotDiceSprites(), dice.getCurrentPilotDiceValues(), touchX, touchY, true)){
+                System.out.println("Pilot dice");
+            }
+            if(dice.isDiceClicked(dice.getCurrentCopilotDiceSprites(), dice.getCurrentCoPilotDiceValues(), touchX, touchY, false)){
+                System.out.println("Copilot dice");
+            }
+        }
         batch.end();
     }
     @Override
     public void resize (int width, int height) {
         viewport.update(width, height, true);
-        batch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
+        //batch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
     }
 }
---
+++ core/src/main/java/com/sierrra/skyTeam/Dice.java
@@ -0,0 +1,99 @@
+package com.sierrra.skyTeam;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Random;
+
+public class Dice {
+    Sprite[] pilotDice;
+    Sprite[] copilotDice;
+    private int[] currentPilotDiceValues;
+    private int[] currentCoPilotDiceValues;
+    private Sprite[] currentPilotDiceSprites;
+    private Sprite[] currentCopilotDiceSprites;
+    //Sprites are better for onclick events (such as the user clicking on the dice to select it)
+    Random random = new Random();
+
+    public Dice() {
+        addSprites();
+        currentPilotDiceSprites = new Sprite[4];
+        currentCopilotDiceSprites = new Sprite[4];
+        currentCoPilotDiceValues = new int[4];
+        currentPilotDiceValues = new int[4];
+    }
+
+    private void addSprites() {
+        pilotDice = new Sprite[6];
+        copilotDice = new Sprite[6];
+        for(int i = 0; i < 6; i++){
+            int diceValue = i+1;
+            pilotDice[i] = new Sprite(new Texture("bluedice_" + diceValue + ".png"));
+            copilotDice[i] = new Sprite(new Texture("orangedice_" + diceValue +".png"));
+        }
+    }
+    public void rollDice() {
+        for(int i = 0; i < 4; i++){
+            //this is to set the current values of the dice. randomizes and picks a number
+            //between 0 and 5. if I get a 0, it will pick the dice with 1 since at index 0 of
+            //xyzDice, the xyzdice_1 img is stored.
+            int pilotDiceValue = random.nextInt(6);
+            currentPilotDiceValues[i] = pilotDiceValue + 1;
+            currentPilotDiceSprites[i] = new Sprite(pilotDice[pilotDiceValue]);
+
+            int copilotDiceValue = random.nextInt(6);
+            currentCoPilotDiceValues[i] = copilotDiceValue + 1;
+            //so it's better to make individual sprites for each or else if we roll the same
+            //number, it'll not have any click functionality
+            currentCopilotDiceSprites[i] = new Sprite(copilotDice[copilotDiceValue]);
+        }
+    }
+    public void renderDice(SpriteBatch batch, boolean isPilot, float startY) {
+        Sprite[] dice = isPilot ? currentPilotDiceSprites : currentCopilotDiceSprites;
+        float startX = isPilot ? 95 : 925;
+        for(int i = 0; i < dice.length; i++){
+            Sprite sprite = dice[i];
+            sprite.setSize(50, 50);
+            sprite.setPosition(startX + i*75, startY);
+            sprite.draw(batch);
+        }
+    }
+    public boolean isDiceClicked(Sprite[] diceSprites, int[] diceValues, float touchX, float touchY, boolean isPilot){
+        for(int i = 0; i < diceSprites.length; i++){
+            Sprite dice = diceSprites[i];
+            if(dice.getBoundingRectangle().contains(touchX, touchY)) {
+                int diceValue = diceValues[i];
+                if(isPilot){
+                    System.out.println("Pilot dice clicked, value: " + diceValue);
+                }else {
+                    System.out.println("Copilot dice clicked, value: " + diceValue);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Sprite[] getCurrentPilotDiceSprites(){
+        return currentPilotDiceSprites;
+    }
+    public Sprite[] getCurrentCopilotDiceSprites() {
+        return currentCopilotDiceSprites;
+    }
+    public int[] getCurrentPilotDiceValues(){
+        return currentPilotDiceValues;
+    }
+    public int[] getCurrentCoPilotDiceValues(){
+        return currentCoPilotDiceValues;
+    }
+
+    public void dispose() {
+        for (Sprite sprite : pilotDice){
+            sprite.getTexture().dispose();
+        }
+        for(Sprite sprite : copilotDice){
+            sprite.getTexture().dispose();
+        }
+    }
+}
