21c8adc2d471acac7ddca416a8fb9eaab610413d
Fadhil Fasalu Rahiman <ffasalurahiman@hof-university.de>
2025-01-07 10:58:03 +0100

    small changes

--- core/src/main/java/com/sierra/skyTeam/controller/GameController.java
+++ core/src/main/java/com/sierra/skyTeam/controller/GameController.java
@@ -1,32 +1,24 @@
 package com.sierra.skyTeam.controller;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.sierra.skyTeam.model.Airplane;
-import com.sierra.skyTeam.model.GameModel;
-import com.sierra.skyTeam.view.CoffeeManager;
-import com.sierra.skyTeam.view.FieldGenerator;
-import com.sierra.skyTeam.view.FieldView;
+import com.sierra.skyTeam.model.*;
+import com.sierra.skyTeam.view.*;
 
 import java.util.List;
 
 public class GameController {
 
     private GameModel gameModel;
-    private Airplane airplaneModel;
 
     private AxisController axisController;
     private PlayerController playerController;
     private DiceController diceController;
 
     private List<FieldView> fieldsView;
-    private CoffeeManager coffeeManager;
 
     public GameController(){
         this.gameModel = new GameModel();
-        this.airplaneModel = gameModel.getAirplane();
-        this.fieldsView = FieldGenerator.generateFields();;
-
-
+        this.fieldsView = FieldGenerator.generateFields();
 
         this.axisController = new AxisController(gameModel);
         this.playerController = new PlayerController(gameModel);
