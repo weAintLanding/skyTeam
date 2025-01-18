0ece4b1181152661542051bde966810daa602a76
naren <nprakash@hof-university.de>
2025-01-18 14:42:57 +0100

    FlapsTest and screenFactory deletion

--- core/src/main/java/com/sierra/skyTeam/screens/ScreenFactory.java
+++
@@ -1,8 +0,0 @@
-package com.sierra.skyTeam.screens;
-
-import com.badlogic.gdx.Screen;
-import com.sierra.skyTeam.MainGame;
-
-public interface ScreenFactory {
-    Screen createCrashScreen(MainGame game);
-}
